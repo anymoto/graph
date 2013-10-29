c3110d9a02a83f2911700f925ff5cec6f7e67bed
anymoto <ana.castro@crowdint.com>
2013-10-29 16:20:24 -0600

    Add nodes and vertex

--- graph.rb
+++ graph.rb
@@ -1,3 +1,15 @@
 class Graph
-  
+  def initialize
+    @nodes = {}
+  end
+
+  def add_vertex(node, edges)
+    #g = Graph.new
+    #g.add_vertex('a', {'b' => 2, 'c' => 1}
+    @nodes[node] = edges
+  end
+
+  def shortest_path(start, finish)
+
+  end
 end
