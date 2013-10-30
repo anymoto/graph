68374f7fd1258aab53db3240c6ad9dd51a6a3081
anymoto <ana.castro@crowdint.com>
2013-10-30 00:20:55 -0600

    Add specs

--- Rakefile
+++ Rakefile
@@ -1,6 +1,11 @@
 require 'rake'
+require 'rspec/core/rake_task'
 
-desc 'Builds a graph'
+namespace :test do
+  RSpec::Core::RakeTask.new :all
+end
+
+desc 'Gets the shortest path for an example graph'
 task :build_graph do
   load './graph.rb'
 
@@ -15,7 +20,7 @@ task :build_graph do
   g.add_vertex('g', {'d' => 3, 'f' => 4, 'c' => 1})
 
   path = g.shortest_path('a', 'f')
-  puts "The shortest path from a to f is #{@path}"
+  puts "The shortest path from a to f is: #{path}"
 
 end  
 
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -2,14 +2,27 @@ require 'rspec'
 require './graph.rb'
 
 describe Graph do
+  let(:graph) { Graph.new }
+  let(:vertices) { Hash.new }
+  let(:a_node) { 'a' }
+  let(:a_edges) { {'b' => 2, 'c' => 3} }
+  let(:g_node) { 'g' }
+  let(:g_edges) { {'d' => 3, 'b' => 1} }
+  let(:start_node) {a_node}
+  let(:finish_node) {g_node}
+
   describe '#add_vertex' do
     it 'creates a graph by adding nodes and vertices' do
-
+      expect(graph.add_vertex(a_node, a_edges)).to change{vertices.size}.by(1)
+      expect(vertices.first[0]).to eql 'a'
+      expect(vertices.first[1]).to be_kind_of Hash
     end
   end
 
   describe '#shortest_path' do
-
+    it 'returns the shortest path' do
+      expect(graph.shortest_path(start_node, finish_node)).to eql 'a => b => d => g'
+    end
   end
 end
 
