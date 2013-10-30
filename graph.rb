8b55f6273c05d4cb7e026dc3b90634e67a12b7f0
anymoto <ana.castro@crowdint.com>
2013-10-29 20:46:15 -0600

    add spec file

--- graph.rb
+++ graph.rb
@@ -1,15 +1,33 @@
 class Graph
   def initialize
-    @nodes = {}
+    @vertices = {}
+    @unvisited = []
   end
 
   def add_vertex(node, edges)
-    #g = Graph.new
-    #g.add_vertex('a', {'b' => 2, 'c' => 1}
-    @nodes[node] = edges
+    @vertices[node] = edges
   end
 
   def shortest_path(start, finish)
+    if validate_nodes
+      path = []
+      current = start
+      path << start
 
+      while current != finish
+        next_node = @vertices[current].sort{|a,b| a[1] <=> b[1]}.first[0]
+        path << next_node
+        current = next_node
+      end
+
+      puts path.inspect
+    end
   end
+
+  private
+
+  def validate_nodes (node, other_node)
+    @vertices.has_key?(node) && @vertices.has_key?(other_node)
+  end
+
 end
---
+++ spec/graph_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './graph.rb'
+
+describe Graph do
+  describe '#add_vertex' do
+    it 'creates a graph by adding nodes and vertices' do
+
+    end
+  end
+
+  describe '#shortest_path' do
+
+  end
+end
+
